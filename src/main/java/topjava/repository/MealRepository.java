e51f801f7ba0a004140e4c4729b925cae776bfc5
Alina <K.alinka555@list.ru>
2022-06-16 08:46:23 +0300

    #HW1.2 add CRUD

--- src/main/java/topjava/repository/MealRepository.java
+++ src/main/java/topjava/repository/MealRepository.java
@@ -15,21 +15,21 @@ public class MealRepository implements IRepository {
     private AtomicInteger counter = new AtomicInteger(0);
 
     {
-            createMeal (new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500));
-            createMeal (new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000));
-            createMeal (new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500));
-            createMeal (new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100));
-            createMeal (new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000));
-            createMeal (new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500));
-            createMeal (new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410));
+        createMeal(new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500));
+        createMeal(new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000));
+        createMeal(new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500));
+        createMeal(new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100));
+        createMeal(new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000));
+        createMeal(new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500));
+        createMeal(new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410));
     }
 
     @Override
     public void createMeal(Meal meal) {
-        if (meal.getId()==null) {
+        if (meal.getId() == null) {
             meal.setId(counter.incrementAndGet());
         }
-        repository.put(meal.getId(),meal);
+        repository.put(meal.getId(), meal);
     }
 
     @Override
@@ -39,14 +39,14 @@ public class MealRepository implements IRepository {
 
     @Override
     public void deleteMeal(String id, Meal meal) {
-        if (meal.getId()!=null){
+        if (meal.getId() != null) {
             repository.remove(meal.getId(), meal);
         }
     }
 
     @Override
     public void updateMeal(String id, Meal meal) {
-        if (meal.getId()!=null){
+        if (meal.getId() != null) {
             repository.put(meal.getId(), meal);
         }
     }
